e427ff0ae92e16a3436fb45ac5be287208e613fd
pratamagates03 <pratamagates03@gmail.com>
2017-11-11 19:10:19 +0700

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "00:00:00.00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(207, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(26, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(201, 42);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Selesai . . .";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Tekan 1-50 berurutan";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(192, 80);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(30, 13);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "reset";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "~ Game TS ~";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 317);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch objStopWatch = new Stopwatch();
+        bool paused = false;
+        int tamp = 0;
+        int[] array;
+        int[] array2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nilairand();
+        }
+
+
+
+        public void createbddsdutton()
+        {
+
+            int top = 108;
+            int k = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                int left = 29;
+                for (int j = 0; j < 5; j++)
+                {
+                    Button button = new Button();
+                    button.Size = new Size(34, 34);
+                    button.Left = left;
+                    button.Top = top;
+                    this.Controls.Add(button);
+                    left += 39;
+                    button.Name = "button" + k;
+                    button.Click += tombolklik;
+                    k++;
+                }
+                top += 39;
+            }
+        }
+
+        private void tombolklik(object sender, EventArgs e)
+        {
+            Button button=sender as Button;
+            button.Text = cek(Int32.Parse(button.Text));
+        }
+
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            if (objStopWatch.IsRunning)
+            {
+                TimeSpan objTimeSpan = TimeSpan.FromMilliseconds(objStopWatch.ElapsedMilliseconds);
+                label2.Text = String.Format(CultureInfo.CurrentCulture, "{0:00}:{1:00}:{2:00}.{3:00}", objTimeSpan.Hours, objTimeSpan.Minutes, objTimeSpan.Seconds, objTimeSpan.Milliseconds / 10);
+                if (paused)
+                {
+                    paused = false;
+                }
+                
+            }
+        }
+
+        string cek(int x)
+        {
+            string angka = Convert.ToString(x);
+            if (x == Int32.Parse(label3.Text) + 1)
+            {
+                label3.Text = Convert.ToString(Int32.Parse(label3.Text) + 1);
+                tamp++;
+                if (x <= 25)
+                    angka = array2[x - 1].ToString();
+                else if (x > 25 && tamp == x)
+                    angka = " ";
+            }
+            if (x == 1) objStopWatch.Start();
+            else if (tamp == 50) { objStopWatch.Stop(); label5.Visible = true; }
+            return angka;
+        }
+
+        
+        public void nilairand()
+        {
+            createbddsdutton();
+            label5.Visible = false;
+            Random random = new Random();
+            array = new int[25];
+            array2 = new int[25];
+            int nom;
+            for (int i = 0; i < 25; i++)
+            {
+                nom = random.Next(1, 26);
+                if (!array.Contains(nom))
+                    array[i] = nom;
+                else
+                    i--;
+            }
+            for (int i = 0; i < 25; i++)
+            {
+                nom = random.Next(26, 51);
+                if (!array2.Contains(nom))
+                    array2[i] = nom;
+                else
+                    i--;
+            }
+
+            for (int i = 1; i < 26; i++)
+            {
+                foreach (Control c in this.Controls)
+                {
+                    if (c is Button && c.Name.EndsWith(i.ToString()))
+                    {
+                        c.Text = array[i - 1].ToString();
+                    }
+
+                }
+            }
+            
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            label2.Text = "00:00:00.00";
+            label3.Text = "0";
+            objStopWatch.Reset();
+            nilairand();
+        }
+
+        
+
+        
+
+        
+    }
+}
+
